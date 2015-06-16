f140734a8ed548f23ceae5ebb2897590dc9b15b3
Stephen <iammrrhodes@gmail.com>
2015-06-16 14:08:13 +0100

    SR - Added in memory mock for mongo for now.

--- db.js
+++ db.js
@@ -1,3 +1,26 @@
-var mongojs = require('mongojs');
+//var mongojs = require('mongojs');
+//module.exports = mongojs('question-time', ['questions', 'moods']);
 
-module.exports = mongojs('question-time', ['questions', 'moods']);
+var collection = function() {
+	
+	var records = [];
+	
+	return {
+		insert: function(data, callback) {
+			records.push(data);
+			callback(null, data);
+		},
+		find: function() {
+			if (arguments.length == 1) {
+				arguments[0](null, records);
+			} else if(arguments.length == 2) {
+				arguments[1](null, records);
+			}
+		}
+	};
+};
+
+module.exports = {
+	questions: collection(),
+	moods: collection()
+};
