c32c73cfbaaf5af69c693183e558d2d4d2e78fa1
David Boyne <davidboyne123@hotmail.co.uk>
2015-06-16 13:39:09 +0100

    DB - Added ui

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/question', function(req, res, next) {
+    res.render('question', {
+        title: 'Express',
+        enabelBackToHome:true
+    });
+});
+
+router.get('/feel', function(req, res, next) {
+    res.render('feeling', {
+        title: 'Express',
+        enabelBackToHome:true
+    });
+});
+
 module.exports = router;
