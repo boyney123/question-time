200e52f7cd532a4c5cc83fd294a412e01bcb5818
David Boyne <davidboyne123@hotmail.co.uk>
2015-06-16 21:58:33 +0100

    went back to memory data

--- db.js
+++ db.js
@@ -1,27 +1,27 @@
-var mongojs = require('mongojs');
-module.exports = mongojs('question-time', ['questions', 'moods']);
+//var mongojs = require('mongojs');
+//module.exports = mongojs('question-time', ['questions', 'moods']);
 
-//var collection = function() {
-//
-//	var records = [];
-//
-//	return {
-//		insert: function(data, callback) {
-//			records.push(data);
-//			callback(null, data);
-//		},
-//		find: function() {
-//			if (arguments.length == 1) {
-//				arguments[0](null, records);
-//			} else if(arguments.length == 2) {
-//				arguments[1](null, records);
-//			}
-//		}
-//	};
-//};
-//
-//module.exports = {
-//	questions: collection(),
-//	moods: collection()
-//};
+var collection = function() {
+
+	var records = [];
+
+	return {
+		insert: function(data, callback) {
+			records.push(data);
+			callback(null, data);
+		},
+		find: function() {
+			if (arguments.length == 1) {
+				arguments[0](null, records);
+			} else if(arguments.length == 2) {
+				arguments[1](null, records);
+			}
+		}
+	};
+};
+
+module.exports = {
+	questions: collection(),
+	moods: collection()
+};
 
